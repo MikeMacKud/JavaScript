0add6b50d323841cffaddcd35b1937fcc71f4d4a
Mikhail Kudashev <komandarmy@rambler.ru>
2022-07-01 19:22:32 +0300

    added simple object operations

---
+++ 004_Object_01/Object_01.js
@@ -0,0 +1,18 @@
+/* Напишите код, выполнив задание из каждого пункта отдельной строкой=
+
+Создайте пустой объект user.
+Добавьте свойство name со значением John.
+Добавьте свойство surname со значением Smith.
+Измените значение свойства name на Pete.
+Удалите свойство name из объекта. */
+
+// create an object
+let user = {};
+// add property 'name'
+user.name = 'John';
+// add property 'surname'
+user.surname = 'Smith';
+// change the value 'name'
+user.name = 'Pete';
+// del property 'name'
+delete user.name;
